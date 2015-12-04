cb49808d0471c1260af6c9324d3058e1fcf3e4ec
Iñigo Alonso Ruiz <inigol22zgz@gmail.com>
2015-12-04 17:52:48 +0100

    cambios app Twitter nMax

--- src/main/java/soa/web/SearchController.java
+++ src/main/java/soa/web/SearchController.java
@@ -6,7 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class SearchController {
@@ -19,10 +20,16 @@ public class SearchController {
         return "index";
     }
 
-
     @RequestMapping(value="/search")
     @ResponseBody
-    public Object search(@RequestParam("q") String q) {
-        return producerTemplate.requestBodyAndHeader("direct:search", "", "CamelTwitterKeywords", q);
+    public Object search(@RequestParam("q") String q,
+		@RequestParam(value = "nMax", required = false) Integer nMax) {
+			Map<String,Object> headers = new HashMap<>();
+			headers.put("CamelTwitterKeywords",q); //Adding the keywords
+			if(nMax != null){
+					 //Adding the number of tweets you want
+					 headers.put("CamelTwitterCount",nMax);
+			 }
+        return producerTemplate.requestBodyAndHeaders("direct:search", "", headers);
     }
 }
